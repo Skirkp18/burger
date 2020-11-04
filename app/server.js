8418e02b86a88db520210051913c6335e9048ffa
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-11-04 18:37:03 -0500

    added lables and cleaned up code

--- app/server.js
+++ app/server.js
@@ -1,24 +1,28 @@
+// DEPENDENCIES
 var express = require("express");
 
 var PORT = process.env.PORT || 8000;
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// STATIC FILES
 app.use(express.static("public"));
 
-// Parse application body
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// HANDLEBARS
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// REQUIRE CONNECTION TO DATABASE
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
+// START SERVER
 app.listen(PORT, function() {
   console.log("Listening on port:%s", PORT);
 });
